fa9a2b30e4511c7313558c7050db7e2f51339948
Roberto Carvelli <robertocarvelli@hotmail.it>
2025-03-07 12:31:33 -0500

    lint fixes

--- tests/test_models.py
+++ tests/test_models.py
@@ -113,7 +113,7 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(Decimal(new_product.price), saved_prod.price)
         self.assertEqual(new_product.available, saved_prod.available)
         self.assertEqual(new_product.category, saved_prod.category)
-    
+
     def test_update_a_product(self):
         """It should Update a Product"""
         product = ProductFactory()
@@ -129,7 +129,7 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(len(products), 1)
         self.assertEqual(products[0].id, original_id)
         self.assertEqual(products[0].description, "updated description")
-    
+
     def test_delete_a_product(self):
         """It should Delete a Product"""
         product = ProductFactory()
@@ -137,18 +137,18 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(len(Product.all()), 1)
         product.delete()
         self.assertEqual(len(Product.all()), 0)
-    
+
     def test_list_all_products(self):
         """It should List all Products in the database"""
         products = Product.all()
         self.assertEqual(products, [])
-        for _ in range(5): 
+        for _ in range(5):
             product = ProductFactory()
             product.id = None
             product.create()
         products = Product.all()
         self.assertEqual(len(products), 5)
-    
+
     def test_find_by_name(self):
         """It should Find a Product by Name"""
         products = ProductFactory.create_batch(5)
@@ -160,7 +160,7 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.name, name)
-    
+
     def test_find_by_availability(self):
         """It should Find Products by Availability"""
         products = ProductFactory.create_batch(10)
